b3aa76b2fae206c06bd14d2a73c067b0784a1615
qtanran <1240429779@qq.com>
2023-07-17 17:44:43 +0800

    feat: createGetter && createSetter

--- packages/vue/dist/vue.js
+++ packages/vue/dist/vue.js
@@ -2,9 +2,62 @@ var Vue = (function (exports) {
     'use strict';
 
     /**
+     * 用于收集依赖的方法
+     * @param target WeakMap 的 Key
+     * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+     */
+    function track(target, key) {
+        console.log('track 收集依赖');
+    }
+    /**
+     * 触发依赖的方法
+     * @param target WeakMap 的 key
+     * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+     * @param newValue 指定 key 的最新值
+     */
+    function trigger(target, key, newValue) {
+        console.log('trigger 触发依赖');
+    }
+
+    /**
+     * getter 回调方法
+     */
+    const get = createGetter();
+    /**
+     * 创建 getter 回调方法
+     */
+    function createGetter() {
+        return function (target, key, receiver) {
+            // 利用 Reflect 得到返回值
+            const res = Reflect.get(target, key, receiver);
+            // 收集依赖
+            track();
+            return res;
+        };
+    }
+    /**
+     * setter 回调方法
+     */
+    const set = createSetter();
+    /**
+     * 创建 setter 回调方法
+     */
+    function createSetter() {
+        return function (target, key, value, receiver) {
+            // 利用 Reflect.set 设置新值
+            const result = Reflect.set(target, key, value, receiver);
+            // 触发依赖
+            trigger();
+            return result;
+        };
+    }
+    /**
      * 响应性的 handler
      */
-    const mutableHandlers = {};
+    const mutableHandlers = {
+        get,
+        set
+    };
 
     /**
      * 响应性 Map 缓存对象

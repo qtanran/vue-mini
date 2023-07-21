492d36901e44831e27e4e532011024192ad41b76
qtanran <1240429779@qq.com>
2023-07-21 11:18:05 +0800

    feat(watch): 初步实现监听器

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -49,6 +49,8 @@ export class ReactiveEffect<T = any> {
     // 执行 fn 函数
     return this.fn()
   }
+
+  stop() {}
 }
 
 type KeyToDepMap = Map<any, Dep>
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -17,6 +17,10 @@ export function reactive(target: object) {
   return createReactiveObject(target, mutableHandlers, reactiveMap)
 }
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive'
+}
+
 /**
  * 创建响应性对象
  * @param target 被代理对象
@@ -36,6 +40,8 @@ function createReactiveObject(
 
   // 未被代理则生成 proxy 实例
   const proxy = new Proxy(target, baseHandlers)
+  // 为 Reactive 增加标识
+  proxy[ReactiveFlags.IS_REACTIVE] = true
   // 缓存代理对象
   proxyMap.set(target, proxy)
   return proxy
@@ -47,3 +53,9 @@ function createReactiveObject(
  */
 export const toReactive = <T extends unknown>(value: T): T =>
   isObject(value) ? reactive(value as object) : value
+
+/**
+ * 判断一个数据是否为 Reactive
+ * @param value
+ */
+export const isReactive = (value): boolean => value?.[ReactiveFlags.IS_REACTIVE]

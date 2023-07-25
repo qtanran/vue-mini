e318be3bff1645accf6a5456694f6084704157f1
qtanran <1240429779@qq.com>
2023-07-25 10:13:09 +0800

    feat(render): ELEMENT 下 style 属性的挂载和更新

--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -2,6 +2,7 @@ import { isOn } from '@vue/shared'
 import { patchClass } from './modules/class'
 import { patchAttr } from './modules/attrs'
 import { patchDOMProp } from './modules/props'
+import { patchStyle } from './modules/style'
 
 /**
  * 为 prop 进行打补丁操作
@@ -14,6 +15,8 @@ export const patchProp = (el, key, prevValue, nextValue) => {
   if (key === 'class') {
     patchClass(el, nextValue)
   } else if (key === 'style') {
+    // style
+    patchStyle(el, prevValue, nextValue)
   } else if (isOn(key)) {
   } else if (shouldSetAsProp(el, key)) {
     // 通过 DOM Properties 指定
